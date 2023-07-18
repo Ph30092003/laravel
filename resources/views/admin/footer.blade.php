f94b030362648c5222f25eb26a334f822889efb2
phanznamz <pnam48689@gmail.com>
2023-07-18 18:32:22 +0700

    Ngày 1 làm asm

---
+++ resources/views/admin/footer.blade.php
@@ -0,0 +1,24 @@
+  <!-- latest jquery-->
+  <script src="/template/admin/assets/js/jquery-3.6.0.min.js"></script>
+
+  <!-- Bootstrap js-->
+  <script src="/template/admin/assets/js/bootstrap/bootstrap.bundle.min.js"></script>
+  <script src="/template/admin/assets/js/bootstrap/popper.min.js"></script>
+
+  <!-- feather icon js-->
+  <script src="/template/admin/assets/js/feather/feather.min.js"></script>
+  <script src="/template/admin/assets/js/feather/feather-icon.js"></script>
+
+  <!-- Slick js-->
+  <script src="/template/admin/assets/js/slick/slick.js"></script>
+  <script src="/template/admin/assets/js/slick/slick-animation.min.js"></script>
+  <script src="/template/admin/assets/js/slick/custom_slick.js"></script>
+
+  <!-- Lazyload Js -->
+  <script src="/template/admin/assets/js/lazysizes.min.js"></script>
+
+  <!-- script js -->
+  <script src="/template/admin/assets/js/script.js"></script>
+
+  <!-- thme setting js -->
+  <script src="/template/admin/assets/js/theme-setting.js"></script>
---
+++ resources/views/admin/users/login.blade.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- Mirrored from themes.pixelstrap.com/fastkart/front-end/login.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 09 Jul 2023 08:11:31 GMT -->
+<head>
+    @include('admin.header');
+</head>
+
+<body>
+<!-- log in section start -->
+    <section class="log-in-section background-image-2 section-b-space">
+        <div class="container-fluid-lg w-100">
+                <div class="col-xxl-4 col-xl-5 col-lg-6 col-sm-8 mx-auto">
+                    <div class="log-in-box">
+                        <div class="log-in-title">
+                            <h3>Welcome To Fastkart</h3>
+                        </div>
+
+                        <div class="input-box">
+                            <form class="row g-4" action="/admin/users/login/store" method="post">
+                                <div class="col-12">
+                                    <div class="form-floating theme-form-floating log-in-form">
+                                        <input type="email" name="email" class="form-control" id="email" placeholder="Email Address">
+                                        <label for="email">Email Address</label>
+                                    </div>
+                                </div>
+
+                                <div class="col-12">
+                                    <div class="form-floating theme-form-floating log-in-form">
+                                        <input type="password" name="password" class="form-control" id="password"
+                                            placeholder="Password">
+                                        <label for="password">Password</label>
+                                    </div>
+                                </div>
+
+                                <div class="col-12">
+                                    <div class="forgot-box">
+                                        <div class="form-check ps-0 m-0 remember-box">
+                                            <input class="checkbox_animated check-box" type="checkbox" name="checkbox"
+                                                id="flexCheckDefault">
+                                            <label class="form-check-label" for="flexCheckDefault">Remember me</label>
+                                        </div>
+                                        <a href="forgot.html" class="forgot-password">Forgot Password?</a>
+                                    </div>
+                                </div>
+
+                                <div class="col-12">
+                                    <button class="btn btn-animation w-100 justify-content-center" type="submit">Log
+                                        In</button>
+                                </div>
+                                @csrf
+                            </form>
+                        </div>
+
+                        <div class="other-log-in">
+                            <h6>or</h6>
+                        </div>
+
+                        <div class="log-in-button">
+                            <ul>
+                                <li>
+                                    <a href="https://www.google.com/" class="btn google-button w-100">
+                                        <img src="../assets/images/inner-page/google.png" class="blur-up lazyload"
+                                            alt=""> Log In with Google
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.facebook.com/" class="btn google-button w-100">
+                                        <img src="../assets/images/inner-page/facebook.png" class="blur-up lazyload"
+                                            alt=""> Log In with Facebook
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div class="other-log-in">
+                            <h6></h6>
+                        </div>
+
+                        <div class="sign-up-box">
+                            <h4>Don't have an account?</h4>
+                            <a href="sign-up.html">Sign Up</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- log in section end -->
+</body>
+    @include('admin.footer');
+
+<!-- Mirrored from themes.pixelstrap.com/fastkart/front-end/login.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 09 Jul 2023 08:11:31 GMT -->
+</html>
